8d7293b1fa7aa9579dce7c7a9f5b9679e078916c
Nazariy Borbiuk <borbiuk.n@gmail.com>
2023-12-28 14:58:53 +0200

    Add percentage track control by Digit hotkeys

--- scripts/content.js
+++ scripts/content.js
@@ -4,7 +4,6 @@ let tracks = [];
 // Flag to track whether the next button has been added to the player.
 let nextButtonAdded = false;
 
-
 // Track ID on the feed page that was paused.
 let feedPauseTrackId = null;
 
@@ -19,6 +18,12 @@ let playFirst = true;
 
 // Object to handle 'collection' and 'wishlist' pages.
 const collection = {
+
+	// Check that url is a 'collection' or 'wishlist' page url.
+	checkUrl: (url) => {
+		const collectionPageRegex = /^https?:\/\/(?:[^./?#]+\.)?bandcamp\.com\/[^/?#]+(?:\/wishlist)?$/i;
+		return collectionPageRegex.test(url.split('?'));
+	},
 
 	// Check if the current track has played more than 99% and play the next track if so.
 	tryPlayNextTrack: () => {
@@ -81,7 +86,65 @@ const collection = {
 		?.getAttribute('data-collect-item')
 		?.substring(1),
 
-	// Object to handle the next track button functionalities
+	// Object to control the percentage of the track being played.
+	percentage: {
+		// Function to play the next track considering the saved playback percentage.
+		playNextTrack: () => {
+			let percentage = collection.percentage.calculateTimePercentage()
+			if (collection.playNextTrack()) {
+				setTimeout(() => {
+					collection.percentage.click(percentage)
+				}, 500);
+			}
+		},
+
+		// Function to simulate a click at a specific percentage within the seek control.
+		click: (percentage) => {
+			// Get the seek control outer element
+			const seekControlOuter = document.querySelector('.seek-control-outer');
+
+			if (seekControlOuter) {
+				// Calculate the x-coordinate for the click event based on the percentage
+				const rect = seekControlOuter.getBoundingClientRect();
+				const x = rect.left + (rect.width * (percentage / 100));
+
+				// Create a new click event
+				const clickEvent = new MouseEvent('click', {
+					bubbles: true,
+					cancelable: true,
+					view: window,
+					clientX: x,
+					clientY: rect.top + (rect.height / 2) // Middle of the element vertically
+				});
+
+				// Dispatch the event to the seek control outer element
+				seekControlOuter.dispatchEvent(clickEvent);
+			}
+		},
+
+		// Function to calculate the playback percentage of the current track
+		calculateTimePercentage: () => {
+			const convertTimeToSeconds = (timeStr) => {
+				const parts = timeStr.split(':');
+				const minutes = parseInt(parts[0], 10);
+				const seconds = parseInt(parts[1], 10);
+				return (minutes * 60) + seconds;
+			};
+
+			// Retrieve the time strings
+			const positionStr = document.querySelector('.pos-dur [data-bind="text: positionStr"]').textContent;
+			const durationStr = document.querySelector('.pos-dur [data-bind="text: durationStr"]').textContent;
+
+			// Convert time strings to seconds
+			const positionInSeconds = convertTimeToSeconds(positionStr);
+			const durationInSeconds = convertTimeToSeconds(durationStr);
+
+			// Calculate the percentage
+			return (positionInSeconds / durationInSeconds) * 100;
+		}
+	},
+
+	// Object to handle the next track button functionalities.
 	nextTrackButton: {
 
 		// Click the next track button.
@@ -165,15 +228,53 @@ const collection = {
 	}
 };
 
+const album = {
+
+	// Check that url is a album or track page url.
+	checkUrl: (url) => {
+		return url.includes('/album/') || url.includes('/track/');
+	},
+
+	playNextTrack: () => {
+		document.querySelector('.nextbutton').click();
+	},
+
+	playPause: () => {
+		document.querySelector('.playbutton')?.click();
+	},
+
+	percentage: {
+		click: (percentage) => {
+			const control = document.querySelector('.progbar_empty');
+			const thumb = document.querySelector('.thumb.ui-draggable');
+
+			const controlRect = control.getBoundingClientRect();
+			const thumbRect = thumb.getBoundingClientRect();
+
+			const fromX = thumbRect.left;
+			const fromY = thumbRect.top + (thumbRect.height / 2);
+			const toX = controlRect.left + (controlRect.width * (percentage / 100)) - (thumbRect.width / 2);
+			const toY = controlRect.top + (controlRect.height / 2);
+
+			utils.drugElement(thumb, fromX, fromY, toX, toY);
+		},
+	}
+};
+
 // Function to initialize tracks based on the current page.
 const initTracks = () => {
 	let collectionsId;
-	if (window.location.href.includes('/wishlist')) {
-		collectionsId = 'wishlist-grid';
-	} else if (window.location.href.includes('/feed')) {
+	const url = window.location.href;
+	if (collection.checkUrl(url)) {
+		collectionsId = url.includes('/wishlist')
+			? 'wishlist-grid'
+			: 'collection-grid';
+	}
+	else if (feed.checkUrl(url)){
 		collectionsId = 'story-list'
-	} else {
-		collectionsId = 'collection-grid';
+	}
+	else {
+		return;
 	}
 
 	const allTracksOnPage = document.getElementById(collectionsId)
@@ -189,7 +290,7 @@ const initTracks = () => {
 			element: x
 		}));
 
-	if (window.location.href.includes('/feed')) {
+	if (feed.checkUrl(url)) {
 		tracks.forEach(x => {
 			x.element.querySelector('.play-button').onclick = () => {
 				lastFeedPlayingTrackId = null;
@@ -201,63 +302,13 @@ const initTracks = () => {
 	}
 }
 
-// Function to play the next track considering the saved playback percentage
-const playNextTrackWithSavedPercentage = () => {
-	let percentage = calculateTimePercentage()
-	if (collection.playNextTrack()) {
-		setTimeout(function () {
-			clickAtPercentageWithinSeekControl(percentage)
-		}, 500)
-	}
-}
-
-// Function to calculate the playback percentage of the current track
-const calculateTimePercentage = () => {
-	function convertTimeToSeconds(timeStr) {
-		const parts = timeStr.split(':');
-		const minutes = parseInt(parts[0], 10);
-		const seconds = parseInt(parts[1], 10);
-		return (minutes * 60) + seconds;
-	}
-
-	// Retrieve the time strings
-	const positionStr = document.querySelector('.pos-dur [data-bind="text: positionStr"]').textContent;
-	const durationStr = document.querySelector('.pos-dur [data-bind="text: durationStr"]').textContent;
-
-	// Convert time strings to seconds
-	const positionInSeconds = convertTimeToSeconds(positionStr);
-	const durationInSeconds = convertTimeToSeconds(durationStr);
-
-	// Calculate the percentage
-	return (positionInSeconds / durationInSeconds) * 100;
-}
-
-// Function to simulate a click at a specific percentage within the seek control
-const clickAtPercentageWithinSeekControl = (percentage) => {
-	// Get the seek control outer element
-	const seekControlOuter = document.querySelector('.seek-control-outer');
-
-	if (seekControlOuter) {
-		// Calculate the x-coordinate for the click event based on the percentage
-		const rect = seekControlOuter.getBoundingClientRect();
-		const x = rect.left + (rect.width * (percentage / 100));
-
-		// Create a new click event
-		const clickEvent = new MouseEvent('click', {
-			bubbles: true,
-			cancelable: true,
-			view: window,
-			clientX: x,
-			clientY: rect.top + (rect.height / 2) // Middle of the element vertically
-		});
-
-		// Dispatch the event to the seek control outer element
-		seekControlOuter.dispatchEvent(clickEvent);
-	}
-}
-
 // Object to handle 'feed' page.
 const feed = {
+
+	// Check that url is a 'feed' page url.
+	checkUrl: (url) => {
+		return url.endsWith('/feed') || url.includes('/feed?')
+	},
 
 	// Try to play the next track in the feed.
 	tryPlayNextTrack: () => {
@@ -345,6 +396,37 @@ const utils = {
 
 	// Check if a value exists (not null, undefined, or empty string).
 	exist: (value) => !utils.notExist(value),
+
+	// Drug element.
+	drugElement: (element, fromX, fromY, toX, toY) => {
+		const down = new MouseEvent('mousedown', {
+			bubbles: true,
+			cancelable: true,
+			view: window,
+			clientX: fromX,
+			clientY: fromY,
+		});
+
+		const move = new MouseEvent('mousemove', {
+			bubbles: true,
+			cancelable: true,
+			view: window,
+			clientX: toX,
+			clientY: toY,
+		});
+
+		const up = new MouseEvent('mouseup', {
+			bubbles: true,
+			cancelable: true,
+			view: window,
+			clientX: toX,
+			clientY: toY,
+		});
+
+		element.dispatchEvent(down);
+		element.dispatchEvent(move);
+		element.dispatchEvent(up);
+	},
 };
 
 // Main function to start the application.
@@ -358,10 +440,10 @@ const main = () => {
 		try {
 			collection.nextTrackButton.addToPlayer();
 
-			const url = window.location.pathname;
-			if (url.includes('/feed')) {
+			const url = window.location.href;
+			if (feed.checkUrl(url)) {
 				feed.tryPlayNextTrack();
-			} else {
+			} else if (collection.checkUrl(url)){
 				collection.tryPlayNextTrack();
 			}
 		} catch (e) {
@@ -387,28 +469,43 @@ document.addEventListener('keydown', (event) => {
 		return;
 	}
 
+	const url = window.location.href;
 	if (event.code === 'Space') { // Play/Pause current track on the 'Space' keydown
 		event.preventDefault();
 
-		const url = window.location.pathname;
-		if (url.includes('/album/') || url.includes('/track/')) {
-			document.querySelector('.playbutton')?.click();
-		} else if (url.includes('/feed')) {
+		if (album.checkUrl(url)) {
+			album.playPause();
+		}
+		else if (feed.checkUrl(url)) {
 			feed.playPause();
-		} else {
+		}
+		else if (collection.checkUrl(url)) {
 			collection.playPause();
 		}
 	} else if (event.code === 'KeyN') { // Play the next track on the 'N' keydown
-		const url = window.location.pathname;
-		if (url.includes('/album/') || url.includes('/track/')) {
-			document.querySelector('.nextbutton').click();
-		} else if (url.includes('/feed')) {
+		if (album.checkUrl(url)) {
+			album.playNextTrack();
+		}
+		else if (feed.checkUrl(url)) {
 			feed.playNextTrack();
-		} else if (!collection.playNextTrack()) {
-			tracks[0].element.click();
+		}
+		else if (collection.checkUrl(url)) {
+			collection.playNextTrack()
 		}
 	} else if (event.code === 'KeyM') { // Play the next track with currently played percentage on the 'M' keydown
-		playNextTrackWithSavedPercentage();
+		if (collection.checkUrl(url)) {
+			collection.percentage.playNextTrack();
+		}
+	} else if (event.code.startsWith('Digit')) { // Play the current track with percentage on the 'Digit' keydown
+		const percentage = Number(event.code.split('Digit')[1]) * 10;
+
+		if (album.checkUrl(url)) {
+			album.percentage.click(percentage);
+		}
+		else if (collection.checkUrl(url)) {
+			event.preventDefault();
+			collection.percentage.click(percentage);
+		}
 	}
 
 	return true;
